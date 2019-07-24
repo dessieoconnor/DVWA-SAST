d83d6e049b3ad92d0fe57e287a593bdf6a66f5b5
dessieoconnor <dessieoconnor@gmail.com>
2019-07-24 15:30:49 +1000

    mysql update

--- login.php
+++ login.php
@@ -3,6 +3,9 @@
 define( 'DVWA_WEB_PAGE_TO_ROOT', '' );
 require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
+$var = $_POST['var'];
+mysql_query("SELECT * FROM sometable WHERE id = $var");
+
 dvwaPageStartup( array( 'phpids' ) );
 
 dvwaDatabaseConnect();
