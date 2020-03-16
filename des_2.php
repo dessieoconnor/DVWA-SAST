8892aebe893eddacb15d3b3f04ca7478cd826e64
dessieoconnor <dessieoconnor@gmail.com>
2020-03-16 11:08:13 +1100

    Create des_2.php

---
+++ des_2.php
@@ -0,0 +1,13 @@
+// a good user's name
+$name = "santa"; 
+$query = "SELECT * FROM customers WHERE username = '$name'";
+echo "Normal: " . $query . "<br />";
+
+// user input that uses SQL Injection
+$name_bad = "' OR 1'"; 
+
+// our MySQL query builder, however, not a very safe one
+$query_bad = "SELECT * FROM customers WHERE username = '$name_bad'";
+
+// display what the new query will look like, with injection
+echo "Injection: " . $query_bad;
